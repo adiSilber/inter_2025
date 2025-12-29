11eb7f2774d7766c7d0f26b045ac0af11d4c52eb
Adi Silberschein <adisi@n120.mcl11.weizmann.ac.il>
2025-12-29 19:49:51 +0200

    change the data point according to mors instructions

--- play/interpetability_data_point.py
+++ play/interpetability_data_point.py
@@ -2,22 +2,19 @@
 class InterpretabilityDataPoint:
     dataset_name: str
     question_id: str
-    question: str
-    correct_answer: str
+    question: list[str]
+    correct_answer: list[str]
     model_name: str
-    model_prompt: str # The prompt template used
-    model_injection_prompt: str # The full prompt sent to the model (including model_injection placeholder)
-    model_injection: str   # The prompt injection content used (Can be empty)
+    model_prompt: list[str] # The prompt template used
+    model_injection_prompt: list[str] # The full prompt sent to the model (including model_injection placeholder)
+    model_injection: list[str]   # The prompt injection content used (Can be empty)
     model_injection_position: int = 0 # The index of the first token of the injection in the response
-    model_response: str
-    model_response_thinking: str # Substring of model_response that contains the model's "thinking"
+    model_response: list[str]
+    model_response_thinking: list[str] # Substring of model_response that contains the model's "thinking"
     judge_model_name: str
-    judge_model_prompt: str
-    judge_response: str # Full text of the judge model's response
+    judge_model_prompt: list[str]
+    judge_response: list[str] # Full text of the judge model's response
     judge_decision: bool
-    aha_moment_finder_model_name: str
-    aha_moment_finder_model_prompt: str
-    aha_moment_finder_response: str # Full text of the aha_moment_finder model's response
-    aha_moment_finder_first_token: int # The index of the first token of the aha moment in the response
-    aha_moment_finder_last_token: int # The index of the last token of the aha moment in the response
-    
+    aha_moment_first_token: int # The index of the first token of the aha moment in the response
+    aha_moment_last_token: int # The index of the last token of the aha moment in the response
+    seed: int = 42 # The seed used for the random number generator
